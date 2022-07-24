654256c57854f7e653fce64c93c01ccd21e86efc
Debak Sen <52042726+debaksen@users.noreply.github.com>
2022-07-24 15:48:31 +0000

    update printSubseq

--- Recursion/printSubseqEqualsX.java
+++ Recursion/printSubseqEqualsX.java
@@ -1,14 +1,16 @@
 import java.util.*;
 
-// Only prints the subsequence who's value equals to X 
 class printSubseqEqualsX {
   public static void main(String args[]) {
-    int[] arr = { 1, 2, 1 };
+    int[] arr = { 1, 2, 1,2,0 };
     List<Integer> res = new ArrayList<Integer>();
     int reqSum = 2; 
-    printSubseq(arr,res,0,0,reqSum);
+    //1 printSubseq(arr,res,0,0,reqSum);
+    //2printOneSubseq(arr,res,0,0,reqSum);
+    System.out.println(countAllSubseqEqualX(arr,res,0,0,reqSum));
   }
 
+  // 1. Only prints the subsequences who's value equals to X 
   public static void printSubseq(int[] arr,List<Integer> res,int idx,int sum, int reqSum) {
 
     if (idx >= arr.length) {
@@ -24,5 +26,46 @@ class printSubseqEqualsX {
     res.remove(res.size() - 1); // take x
     sum-=arr[idx];
     printSubseq(arr, res,idx + 1,sum,reqSum);
+  }
+
+  // 2. Print only one subsequence* who's value equals to X 
+  public static Boolean printOneSubseq(int[] arr,List<Integer> res,int idx,int sum, int reqSum) {
+
+    if (idx >= arr.length) {
+      if(sum == reqSum){
+        System.out.println(res);
+        return true;
+      }
+      return false;
+    }
+
+    res.add(arr[idx]); // take
+    sum+=arr[idx];
+    if(printOneSubseq(arr, res,idx + 1,sum,reqSum) == true) {return true;}
+    res.remove(res.size() - 1); // take x
+    sum-=arr[idx];
+    if(printOneSubseq(arr, res,idx + 1,sum,reqSum) == true) {return true;}
+
+    return false;
+  }
+  
+// 3. Count all subsequence* who's value equals to X 
+   public static int countAllSubseqEqualX(int[] arr,List<Integer> res,int idx,int sum, int reqSum)   {
+
+    if (idx >= arr.length) {
+      if(sum == reqSum){
+        return 1; 
+      }
+      return 0;
+    }
+
+    res.add(arr[idx]); // take
+    sum+=arr[idx];
+    int cntWithEle = countAllSubseqEqualX(arr, res,idx + 1,sum,reqSum);
+    res.remove(res.size() - 1); // take x
+    sum-=arr[idx];
+    int cntWithOutEle = countAllSubseqEqualX(arr, res,idx + 1,sum,reqSum);
+
+    return cntWithEle + cntWithOutEle; 
   }
 }
